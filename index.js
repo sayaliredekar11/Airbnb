bf62b909e2c306e784df6993a98a6af1b366911e
sayaliredekar11 <sayaliredekar11@gmail.com>
2026-06-20 20:14:00 +0530

    New changes are added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const userRouter = require("./routes/user.js");
 const dbUrl = process.env.ATLASDB_URL;
-
+const expressLayouts = require("express-ejs-layouts");
 main().then(() =>{
     console.log("connected to DB");
 })
